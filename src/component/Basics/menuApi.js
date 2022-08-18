78f7e91d018cee21663e4e2de39d8ce63741a106
chetansharma <chetansharma119821@gmail.com>
2022-08-18 13:52:35 +0530

    first commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,187 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "maggi",
+    category: "breakfast",
+    price: "12₹",
+    description:
+      "Maggi noodles are dried noodles fused with oil, and sold with a packet of flavorings. These noodles are usually eaten after being cooked in boiling water for 3 to 5 minutes or eaten straight from the packet. ",
+  },
+
+  {
+    id: 2,
+    image: "images/noodles.jpg",
+    name: "Noodles",
+    category: "breakfast",
+    price: "20₹",
+    description:
+      "Noodles are a type of food made from unleavened dough which is rolled flat and cut, stretched or extruded, into long strips or strings. Noodles can be refrigerated for short-term storage or dried and stored for future use ",
+  },
+  {
+    id: 3,
+    image: "images/paasta.jpg",
+    name: "Pasta",
+    category: "breakfast",
+    price: "20₹",
+    description:
+      "Pasta is a type of food made from a mixture of flour, eggs, and water that is formed into different shapes and then boiled. Spaghetti, macaroni, and noodles are types of pasta. ",
+  },
+  {
+    id: 4,
+    image: "../images/sandwich.jpg",
+    name: "Sandwich",
+    category: "breakfast",
+    price: "25₹",
+    description:
+      "sandwich, in its basic form, slices of meat, cheese, or other food placed between two slices of bread. Although this mode of consumption must be as old as meat and bread. ",
+  },
+  {
+    id: 5,
+    image: "../images/pizza.jpg",
+    name: "Pizza",
+    category: "lunch",
+    price: "80₹",
+    description:
+      "sandwich, in its basic form, slices of meat, cheese, or other food placed between two slices of bread. Although this mode of consumption must be as old as meat and bread . ",
+  },
+  {
+    id: 6,
+    image: "../images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+  {
+    id: 7,
+    image: "../images/sweets.jpg",
+    name: "Sweets",
+    category: "evening",
+    price: "60₹",
+    description:
+      "Sugary or sweet means syrupy, candied, sugar coated, honeyed, sweetened, sugared, maybe saccharine; opposite to bitter, unsweetened or sugarless. ",
+  },
+  {
+    id: 8,
+    image: "../images/rajmarice.jpg",
+    name: "Rajma Rice",
+    category: "lunch",
+    price: "60₹",
+    description:
+      " Rajma chawal is a rice dish where cooked rice is mixed with rajma masala or kidney beans masala. A comfort food that comes together in a jiffy. ",
+  },
+  {
+    id: 9,
+    image: "../images/samosa.jpg",
+    name: "Samosa",
+    category: "lunch",
+    price: "10₹",
+    description:
+      " A samosa (/səˈmoʊsə/) or singara is a fried or baked pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas. ",
+  },
+  {
+    id: 10,
+    image: "../images/vegthali.jpg",
+    name: "Veg-Thali",
+    category: "lunch",
+    price: "100₹",
+    description:
+      "a grain: rice or flatbread made with wheat, millet, etc. lentils: dal or sambar. vegetables: a seasonal preparation. chutney: a condiment made with fruit, herbs, spices. ",
+  },
+  {
+    id: 11,
+    image: "../images/momos.jpg",
+    name: "Momos",
+    category: "evening",
+    price: "50₹",
+    description:
+      "  Momos are a popular street food in northern parts of India. These are also known as Dim Sum . ",
+  },
+  {
+    id: 12,
+    image: "../images/idli dosa.jpg",
+    name: "Idli Dosa",
+    category: "evening",
+    price: "70₹",
+    description:
+      " Idli is a savory rice cake, made by steaming a batter consisting of lentils and rice. Dosa is a crispy crepe made with the same batter of ... ",
+  },
+  {
+    id: 13,
+    image: "../images/burger.jpg",
+    name: "Burger",
+    category: "evening",
+    price: "90₹",
+    description:
+      "A burger is a flat round mass of minced meat or vegetables, which is fried and often eaten in a bread roll. ",
+  },
+  {
+    id: 14,
+    image: "../images/aloo puri.jpg",
+    name: "Aloo Puri",
+    category: "dinner",
+    price: "60₹",
+    description:
+      " Aloo Puri is a favorite breakfast at my hubby's place for Sunday mornings. Here aloo refers to potato curry and puri is Indian fried bread. ",
+  },
+  {
+    id: 15,
+    image: "../images/biryani.jpg",
+    name: "Biryani",
+    category: "dinner",
+    price: "150₹",
+    description:
+      " biryani is a spiced mix of meat and rice, traditionally cooked over an open fire in a leather pot. It is combined in different ways with a variety of components to create a number of highly tasty and unique flavor combinations. ",
+  },
+  {
+    id: 16,
+    image: "../images/dal-bati-churma.jpg",
+    name: "Dal Bati Churma",
+    category: "dinner",
+    price: "150₹",
+    description:
+      " Dal Baati is a popular Rajasthani dish consisting of mainly Uradh Dal (combination of five lentils) and Baati i.e. small wheat bread balls. ",
+  },
+  {
+    id: 17,
+    image: "../images/chole bhature.jpg",
+    name: "Chole Bhature",
+    category: "evening",
+    price: "50₹",
+    description:
+      " It is a combination of chana masala (spicy white chickpeas) and bhatura/puri, a deep-fried bread made from maida. ",
+  },
+  {
+    id: 18,
+    image: "../images/pau bhaji.jpg",
+    name: "Pau Bhaji",
+    category: "dinner",
+    price: "90₹",
+    description:
+      "Pav bhaji is an Indian fast food consisting of a spicy vegetable gravy served with soft dinner rolls. ",
+  },
+  {
+    id: 19,
+    image: "../images/bread butter.jpg",
+    name: "Bread Butter",
+    category: "breakfast",
+    price: "70₹",
+    description:
+      " Bread and butter is a superstitious blessing or charm, typically said by young couples or friends walking together when they are forced to separate by an obstacle, such as a pole or another person ",
+  },
+
+  {
+    id: 20,
+    image: "../images/fish curry.jpg",
+    name: "Fish Curry",
+    category: "lunch",
+    price: "100₹",
+    description:
+      "Fish curry made with fresh fish, onions, tomatoes, coconut, spices and herbs. Make the easiest, delicious and flavorsome fish curry with my easy step by ...",
+  },
+
+];
+
+export default Menu;
